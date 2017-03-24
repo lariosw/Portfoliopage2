97578a08a05cff51d5c03687b5287d2196696a65
lariosw <lariosw@seattleu.edu>
2017-03-24 12:12:34 -0700

    major style and layout changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,32 @@
-var wlApp = {
+/*var wlApp = {
   openInNewTab: function(url){
     var win = window.open(url, '_blank');
     win.focus();
   }
 };
+*/
+
+function onPageReady() {
+  var $backgroundElement = $("#homepage-background");
+  var $window = $(window);
+  var $arrow = $(".fa-angle-double-up");
+  //initially hide arrow
+  $arrow.hide();
+
+
+  //listen to window scroll for visibility
+  $window.scroll(function() {
+    var heightOfElement= $backgroundElement.offset().top + $backgroundElement.outerHeight();
+    var topOfScreen = $window.scrollTop();
+
+    if((topOfScreen < heightOfElement)){
+      $arrow.hide();
+    }
+    else {
+      $arrow.show();
+    }
+  });
+}
+
+
+$(document).ready(onPageReady);
